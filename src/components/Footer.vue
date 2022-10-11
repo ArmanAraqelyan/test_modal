5ee4cd4386b59b73512faaa1100cdd2c0ff89151
arman20 <arakelyan@ujin.tech>
2022-10-11 17:54:47 +0400

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer>
+  </footer>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Header head_text="not_found" content_text="" page=""/>
+    <h1>page not found</h1>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Header, Footer
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
